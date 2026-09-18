72f4e4cfa866c9bbdb65f9527b3b92cdee44e768
shayd <shayden@tcrobotics.org>
2026-09-17 20:32:15 -0400

    changed some lines to make sure the code worked on drivetrain such as motor direction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MechanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MechanumDrive.java
@@ -4,6 +4,7 @@ and then outputs(power to motors)
 package org.firstinspires.ftc.teamcode.mechanisms;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class MechanumDrive {
@@ -14,14 +15,17 @@ public class MechanumDrive {
     private DcMotor backRight;
 
     public void init(HardwareMap hardwareMap) {
-        frontLeft = hardwareMap.get(DcMotor.class, "front_left_motor");
-        frontRight = hardwareMap.get(DcMotor.class, "front_right_motor");
-        backLeft = hardwareMap.get(DcMotor.class, "back_left_motor");
-        backRight = hardwareMap.get(DcMotor.class, "back_right_motor");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
 
 
-        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+
         backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
     }
 
     public void drive(double drive, double strafe, double turn) {
@@ -29,12 +33,12 @@ public class MechanumDrive {
         //Driving sideways usually takes more power than driving forwards
         strafe = strafe * 1.25;
 
-        double SpeedMultiplier = 1;
+        double SpeedMultiplier = 0.75;
 
-        double flPower = (drive + strafe + turn) * SpeedMultiplier;
-        double frPower = (drive - strafe - turn) * SpeedMultiplier;
-        double blPower = (drive - strafe + turn) * SpeedMultiplier;
-        double brPower = (drive + strafe - turn) * SpeedMultiplier;
+        double flPower = (drive - strafe - turn) * SpeedMultiplier;
+        double frPower = (drive + strafe + turn) * SpeedMultiplier;
+        double blPower = (drive + strafe - turn) * SpeedMultiplier;
+        double brPower = (drive - strafe + turn) * SpeedMultiplier;
 
         // Normalize the values so no wheel power exceeds 100%
         double max = Math.max(Math.abs(flPower), Math.abs(frPower));
