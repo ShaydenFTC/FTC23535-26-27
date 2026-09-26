2993fc9e71977d424c884f69cddf7a5f63e55035
Murali <muralirajeshkumar42@gmail.com>
2026-09-26 14:56:42 -0400

    completed intake for robot once ready

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Intake.java
@@ -6,23 +6,24 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class Intake {
 
     private DcMotor intake;
-
+//initiates intake motor
     public void init(HardwareMap hardwareMap) {
         intake = hardwareMap.get(DcMotor.class, "intake");
         intake.setDirection(DcMotor.Direction.FORWARD);
     }
-
+//intiates code
    public void runIntake() {
         intake.setPower(1.0);
-
+//runs intake
 
     } public void runOuttake() {
         intake.setPower(-1.0);
-
+//runs outake
 
 
 
     } public void stopIntake() {
         intake.setPower(0.0);
+        //stops the intake motor
     }
 }
